12e39f47f05874abc331080a7011c1d80ad8b74d
Мария Лихачева <118101519+MariyaLixa4eva@users.noreply.github.com>
2024-12-24 15:26:38 +0700

    Add files via upload

---
+++ woman/data.js
@@ -0,0 +1,39 @@
+let songs = [
+    {
+        name: 'Rock (Feat. Filapine)',
+        path: './music/Akon - Rock (Feat. Filapine).mp3',
+        artist: 'Akon',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Superstar',
+        path: './music/Cypress Hill - Superstar.mp3',
+        artist: 'Cypress Hill',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Off With Her Head',
+        path: './music/Icon For Hire - Off With Her Head.mp3',
+        artist: 'Icon For Hire',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Ring Frei',
+        path: './music/Lafee - Ring Frei.mp3',
+        artist: 'Lafee',
+        cover: './disk.jpg'
+    },
+    
+    {
+        name: 'The 7 Minute Workout',
+        path: './music/Tabata Songs - The 7 Minute Workout.mp3',
+        artist: 'Tabata Songs',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Mambo',
+        path: './music/Tropkillaz - Mambo.mp3',
+        artist: 'Tropkillaz',
+        cover: './disk.jpg'
+    }
+];
