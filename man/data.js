83a6ca4c4d4c3385900654a96bfa536b46ff8cf1
Мария Лихачева <118101519+MariyaLixa4eva@users.noreply.github.com>
2024-12-24 15:13:19 +0700

    Add files via upload

---
+++ man/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Where The Hood At',
+        path: './music/DMX - Where The Hood At.mp3',
+        artist: 'DMX',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Handzup',
+        path: './music/Raptile Feat. Da Lioness  Cronite - Handzup.mp3',
+        artist: 'Raptile Feat. Da Lioness  Cronite',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Herro',
+        path: './music/Skillet - Herro.mp3',
+        artist: 'Skillet',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'The Only',
+        path: './music/Static X - The Only.mp3',
+        artist: 'Static X',
+        cover: './disk.jpg'
+    },
+    {
+        name: 'Cok Bak Da Hamma!',
+        path: './music/Tha Mexakinz - Cok Bak Da Hamma!.mp3',
+        artist: 'Tha Mexakinz',
+        cover: './disk.jpg'
+    }
+];
